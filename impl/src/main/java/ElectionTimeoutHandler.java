6fec7df7c5fba15136ceb503d8efa078c840100f
Brandon Callender <bmcallender@cs.uchicago.edu>
2016-05-25 21:46:16 -0500

    added election timeout

---
+++ impl/src/main/java/ElectionTimeoutHandler.java
@@ -0,0 +1,10 @@
+/**
+ * Should control the progression of a FOLLOWER to a CANDIDATE following an election timeout.
+ */
+public class ElectionTimeoutHandler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
--- impl/src/main/java/Node.java
+++ impl/src/main/java/Node.java
@@ -1,36 +1,68 @@
 import org.json.JSONObject;
 import org.zeromq.ZMsg;
 
+import java.io.Serializable;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by techbar on 5/25/16.
  */
-public class Node {
+public class Node implements Serializable {
+
+    private static final int HEARTBEAT_INTERVAL = 50;
 
     private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    List<Entry> log;
+    //volatile on all
+    int commitIndex;
+    int lastApplied;
+    Role role;
+    //volatile on master
+    int nextIndex;
+    int matchIndex;
     private BrokerManager brokerManager;
     private Map<String, String> store;
     private String nodeName;
     private boolean connected;
+    //persistent
+    private int currentTerm;
+    private String votedFor;
 
     public Node(String nodeName, BrokerManager brokerManager) {
         this.nodeName = nodeName;
         this.store = new HashMap<>();
         this.brokerManager = brokerManager;
         this.connected = false;
+        this.currentTerm = 0;
+        this.votedFor = null;
+        this.log = new ArrayList<>();
+        this.commitIndex = 0;
+        this.lastApplied = 0;
+        this.role = Role.FOLLOWER;
+        this.nextIndex = 0;
+        this.matchIndex = 0;
 
-        final ScheduledFuture<?> heartBeatTimeout =
+
+        int heartBeatTimeoutValue = ThreadLocalRandom.current().nextInt(150, 301);
+
+        final ScheduledFuture<?> heartBeatSend =
                 this.executorService.scheduleAtFixedRate(new HeartbeatSender(brokerManager),
-                        175,
-                        175,
+                        HEARTBEAT_INTERVAL,
+                        HEARTBEAT_INTERVAL,
                         TimeUnit.MILLISECONDS);
+
+        final ScheduledFuture electionTimeout =
+                this.executorService.scheduleAtFixedRate(new ElectionTimeoutHandler(),
+                        heartBeatTimeoutValue,
+                        heartBeatTimeoutValue,
+                        TimeUnit.MILLISECONDS);
+        brokerManager.logDebug(String.format("Election timeout value for %s is $d", nodeName, heartBeatTimeoutValue));
+        heartBeatSend.cancel(true);
     }
 
     public void handleMessage(ZMsg message) {
@@ -92,4 +124,6 @@ public class Node {
             case UNKNOWN:
         }
     }
+
+    private enum Role {FOLLOWER, CANDIDATE, LEADER}
 }
